ff62fa8d54e335653f6b081094c185254cb04f8f
Josias Junior <josias.junior@quintoandar.com.br>
2022-06-08 20:19:50 -0300

    Entendendo escopo das váriaveis

---
+++ compilador/compilador.js
@@ -0,0 +1,12 @@
+"use strict";
+const canal = 'Eu Sou Satoshi';
+const inscritos = 1000;
+// canal = inscritos
+console.log(`Canal = ${canal}`);
+console.log(`Canal = ${inscritos}`);
+// let nome = "Satoshi"
+// nome = 'Satoshi'
+console.log(`Nome = ${nome}`);
+(() => {
+    let nome = 'Ana';
+});
---
+++ compilador/compilador.ts
@@ -0,0 +1,16 @@
+const canal: string = 'Eu Sou Satoshi'
+const inscritos: number = 112
+
+// canal = inscritos
+
+console.log(`Canal = ${canal}`)
+console.log(`Canal = ${inscritos}`)
+
+let nome = "Satoshi"
+// nome = 'Satoshi'
+console.log(`Nome = ${nome}`);
+
+(()=> {
+    let nome: string = 'Ana'
+    console.log(`Nome = ${nome}`);    
+})
