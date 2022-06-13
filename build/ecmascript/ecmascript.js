4e915d0cd45cb6a27afa6f8eba9beb2293595613
Josias Junior <josias.junior@quintoandar.com.br>
2022-06-13 20:44:11 -0300

    Arrow function

---
+++ build/ecmascript/ecmascript.js
@@ -0,0 +1,38 @@
+"use strict";
+// let & const
+// console.log(seraQuePode)
+let seraQuePode = '?';
+console.log(seraQuePode);
+let estaFrio = true;
+if (estaFrio) {
+    let acao = 'Colocar casaco';
+    console.log(acao);
+}
+// console.log(acao)
+const cpf = '123.456.007-89';
+// cpf = '155.555.666-55'
+console.log(cpf);
+function somar(a, b) {
+    return a + b;
+}
+console.log(somar(1, 5));
+const subtrair = (a, b) => a - b;
+console.log(subtrair(1, 5));
+const saldacao = () => console.log('Olá!');
+saldacao();
+// const falarCom = pessoa: string => console.log('Olá ' + pessoa)
+const falarCom = (pessoa) => console.log('Olá ' + pessoa);
+falarCom('João');
+function normalComThis() {
+    console.log(this);
+}
+normalComThis();
+const normalComThisEspecial = normalComThis.bind({ nome: 'José' });
+normalComThisEspecial();
+// contexto lexico
+console.log(this);
+const arrowComThis = () => console.log(this);
+arrowComThis();
+const arrowComThisEspecial = arrowComThis.bind({ nome: 'Maria' });
+arrowComThisEspecial();
+//# sourceMappingURL=ecmascript.js.map
--- ecmascript/ecmascript.ts
+++ ecmascript/ecmascript.ts
@@ -20,3 +20,34 @@ const cpf: string = '123.456.007-89'
 
 console.log(cpf)
 
+function somar(a: number, b: number): number {
+    return a + b
+}
+console.log(somar(1,5))
+
+const subtrair = (a: number, b: number): number => a - b
+console.log(subtrair(1,5))
+
+const saldacao = () => console.log('Olá!')
+saldacao()
+
+// const falarCom = pessoa: string => console.log('Olá ' + pessoa)
+const falarCom = (pessoa: string) => console.log('Olá ' + pessoa)
+falarCom('João')
+
+function normalComThis(){
+    console.log(this)
+}
+normalComThis()
+
+const normalComThisEspecial = normalComThis.bind({nome: 'José'})
+normalComThisEspecial()
+
+// contexto lexico
+console.log(this)
+const  arrowComThis = () => console.log(this)
+arrowComThis()
+
+const  arrowComThisEspecial = arrowComThis.bind({nome: 'Maria'})
+arrowComThisEspecial()
+
