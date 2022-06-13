582a4b822982f2c8c22da7231a296295ed40f8f3
Josias Junior <josias.junior@quintoandar.com.br>
2022-06-13 20:08:13 -0300

    const e let

---
+++ ecmascript/ecmascript.ts
@@ -0,0 +1,22 @@
+// let & const
+
+// console.log(seraQuePode)
+let seraQuePode = '?'
+console.log(seraQuePode)
+
+
+let estaFrio = true
+
+if(estaFrio){
+    let acao = 'Colocar casaco'
+    console.log(acao)
+}
+// console.log(acao)
+
+
+const cpf: string = '123.456.007-89'
+
+// cpf = '155.555.666-55'
+
+console.log(cpf)
+
