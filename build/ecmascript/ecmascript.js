a49795b945586e6d35b74825ae2cb811c6abaf10
Josias Junior <josias.junior@quintoandar.com.br>
2022-06-18 16:55:32 -0300

    Build

--- build/ecmascript/ecmascript.js
+++ build/ecmascript/ecmascript.js
@@ -1,15 +1,15 @@
 "use strict";
 // let & const
 // console.log(seraQuePode)
-let seraQuePode = '?';
+let seraQuePode = "?";
 console.log(seraQuePode);
 let estaFrio = true;
 if (estaFrio) {
-    let acao = 'Colocar casaco';
+    let acao = "Colocar casaco";
     console.log(acao);
 }
 // console.log(acao)
-const cpf = '123.456.007-89';
+const cpf = "123.456.007-89";
 // cpf = '155.555.666-55'
 console.log(cpf);
 function somar(a, b) {
@@ -18,11 +18,11 @@ function somar(a, b) {
 console.log(somar(1, 5));
 const subtrair = (a, b) => a - b;
 console.log(subtrair(1, 5));
-const saldacao = () => console.log('Olá!');
+const saldacao = () => console.log("Olá!");
 saldacao();
 // const falarCom = pessoa: string => console.log('Olá ' + pessoa)
-const falarCom = (pessoa) => console.log('Olá ' + pessoa);
-falarCom('João');
+const falarCom = (pessoa) => console.log("Olá " + pessoa);
+falarCom("João");
 // function normalComThis(){
 //     console.log(this)
 // }
@@ -47,8 +47,8 @@ contagemRegressiva(3);
 //rest & spread
 const numbers = [1, 10, 55, -5];
 console.log(Math.max(...numbers));
-const turmaA = ['Maria', 'João', 'José'];
-const turmaB = ['Priscila', 'Carla', ...turmaA, 'Fernanda'];
+const turmaA = ["Maria", "João", "José"];
+const turmaB = ["Priscila", "Carla", ...turmaA, "Fernanda"];
 console.log(turmaA);
 console.log(turmaB);
 function retornaArray(...args) {
@@ -58,7 +58,7 @@ const numeros = retornaArray(5, 6, 8, 1, 22, 79, 23, 589);
 console.log(numeros);
 console.log(retornaArray(...numbers));
 // rest & spread (tupla)
-const tupla = [1, 'abc', true];
+const tupla = [1, "abc", true];
 function tuplaParam1(a, b, c) {
     console.log(`1) ${a} ${b} ${c}`);
 }
@@ -68,4 +68,29 @@ function tuplaParam2(...params) {
     console.log(`2) ${params[0]} ${params[1]} ${params[2]}`);
 }
 tuplaParam2(...tupla);
+// destructuring (array)
+const caracteristicas = ["Motor Zetec 1.8", 2020];
+// const motor = caracteristicas[0]
+// const ano = caracteristicas[1]
+const [motor, ano] = caracteristicas;
+console.log(motor);
+console.log(ano);
+// destructuring (object)
+const item = {
+    nome: "Placa de vídeo 3060",
+    preco: 2000,
+    caracteristica: {
+        w: 555,
+    },
+};
+const { nome: descricao, preco: valor, caracteristica: { w }, } = item;
+console.log(descricao);
+console.log(valor);
+// template string
+const usuarioId = "Usuário";
+const notificacoes = 9;
+// const boasVindas = "Boas vindas " + usuarioId + " Notificações: " + notificacoes;
+// console.log(boasVindas)
+const boasVindas = `Boas vindas ${usuarioId} Notificações: ${notificacoes}`;
+console.log(boasVindas);
 //# sourceMappingURL=ecmascript.js.map
