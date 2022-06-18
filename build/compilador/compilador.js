349ed459bdcbdf9dda93f49aac7db3ba8dbdffb5
Josias Junior <josias.junior@quintoandar.com.br>
2022-06-18 16:59:02 -0300

    Fix error TS2451: Cannot redeclare block-scoped variable 'nome'.

--- build/compilador/compilador.js
+++ build/compilador/compilador.js
@@ -1,14 +1,14 @@
 "use strict";
-const canal = 'Eu Sou Satoshi';
+const canal = "Eu Sou Satoshi";
 const inscritos = 112;
 // canal = inscritos
 console.log(`Canal = ${canal}`);
 console.log(`Canal = ${inscritos}`);
-let nome = "Satoshi";
+// let nome = "Satoshi"
 // nome = 'Satoshi'
-console.log(`Nome = ${nome}`);
-(() => {
-    let nome = 'Ana';
+// console.log(`Nome = ${nome}`);
+() => {
+    let nome = "Ana";
     console.log(`Nome = ${nome}`);
-});
+};
 //# sourceMappingURL=compilador.js.map
--- compilador/compilador.ts
+++ compilador/compilador.ts
@@ -1,16 +1,16 @@
-const canal: string = 'Eu Sou Satoshi'
-const inscritos: number = 112
+const canal: string = "Eu Sou Satoshi";
+const inscritos: number = 112;
 
 // canal = inscritos
 
-console.log(`Canal = ${canal}`)
-console.log(`Canal = ${inscritos}`)
+console.log(`Canal = ${canal}`);
+console.log(`Canal = ${inscritos}`);
 
-let nome = "Satoshi"
+// let nome = "Satoshi"
 // nome = 'Satoshi'
-console.log(`Nome = ${nome}`);
+// console.log(`Nome = ${nome}`);
 
-(()=> {
-    let nome: string = 'Ana'
-    console.log(`Nome = ${nome}`);    
-})
+() => {
+  let nome: string = "Ana";
+  console.log(`Nome = ${nome}`);
+};
