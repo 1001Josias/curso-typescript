370258c092d5a87f4d4dabc5fd811b4db3f7f466
Josias Junior <josias.junior@quintoandar.com.br>
2022-07-21 21:28:01 -0300

    Uso de interface para estender Object

--- interfaces/interfaces.ts
+++ interfaces/interfaces.ts
@@ -89,3 +89,28 @@ abstract class AbstrataABD implements A, B {
   b(): void {}
   abstract d(): void;
 }
+
+interface Object {
+  log(): void;
+}
+
+Object.prototype.log = function () {
+  console.log(this.toString());
+};
+
+let x = 2;
+let y = 3;
+let z = 4;
+
+x.log();
+y.log();
+z.log();
+
+let cli = {
+  nome: "Pedro",
+  toString() {
+    return this;
+  },
+};
+
+cli.log();
